f66bce57349400ed824768fbbe3fa701353af5d7
ilife5 <ilife5@qq.com>
2014-12-30 17:56:33 +0800

    add fekit && bower test

---
+++ test/test-app-bower.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+var os = require('os');
+
+describe('avalon-oniui:app bower', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(os.tmpdir(), './temp-test'))
+      .withOptions({ 'skip-install': true })
+      .withPrompt({
+        packageManagement: "bower",
+        appName: "avalon_oniui_yo"
+      })
+      .on('end', done);
+  });
+
+  it('creates files', function () {
+    assert.file([
+      'bower.json',
+      'package.json'
+    ]);
+  });
+});
--- test/test-app-fekit.js
+++ test/test-app-fekit.js
@@ -5,21 +5,21 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
 
-describe('avalon-oniui:app', function () {
-  this.timeout(15000);
+describe('avalon-oniui:app fekit', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
-        someOption: true
+        packageManagement: "fekit",
+        appName: "avalon_oniui_yo"
       })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'bower.json',
+      'fekit.config',
       'package.json'
     ]);
   });
